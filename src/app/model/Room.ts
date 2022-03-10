fd48df556f1f8a428dfa2d98171b3ab4ee056f3c
askild85 <askildar@yandex.ru>
2022-03-10 20:57:41 +0500

    Рефакторинг. Подготовка

--- src/app/model/Room.ts
+++ src/app/model/Room.ts
@@ -1,16 +1,19 @@
+import { BookDates } from "./BookRoomData";
+
 export class Room {
   id: number
   roomNum: number
   places: number
   price: number | string;
   bookPeriod: BookPeriod
-  constructor(id: number, roomNum: number,places: number, price: number | string, bookPeriod: BookPeriod) {
+  bookDates?: BookDates
+  constructor(id: number, roomNum: number,places: number, price: number | string, bookPeriod: BookPeriod, bookDates: BookDates) {
     this.id = id
     this.roomNum = roomNum
     this.places = places
     this.price = price
     this.bookPeriod = bookPeriod
-
+    this.bookDates = bookDates
   }
 }
 
--- src/app/room-list/room-list.component.ts
+++ src/app/room-list/room-list.component.ts
@@ -1,10 +1,13 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+import { MatDialog } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { BookDates, BookRoomData } from '../model/BookRoomData';
 import { BookPeriod, Room } from '../model/Room';
+import { RoomBookComponent } from '../room-book/room-book.component';
 import { HttpService } from './../services/http.service';
 
 
@@ -18,10 +21,8 @@ import { HttpService } from './../services/http.service';
 
 
 export class RoomListComponent implements OnInit, AfterViewInit {
-
   filterDictionary = new Map<string, string>();
-  rooms = [];
-
+  rooms!: Room[];
   displayedColumns: string[] = ['num', 'places', 'price'];
   dataSource!: MatTableDataSource<Room>;
 
@@ -32,66 +33,75 @@ export class RoomListComponent implements OnInit, AfterViewInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
   @ViewChild(MatSort) sort!: MatSort ;
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService,
+              public dialog: MatDialog) {
     // Create 40 rooms
-    const rooms = Array.from({ length: 40 }, (_, k) => createNewRooms(k + 1));
+    // const rooms = Array.from({ length: 40 }, (_, k) => createNewRooms(k + 1));
     // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(rooms);
+    //
   }
 
   ngAfterViewInit() {
+    this.dataSource = new MatTableDataSource(this.rooms);
     this.dataSource.paginator = this.paginator || null;
     this.dataSource.sort = this.sort;
- }
+    this.dataSource.filterPredicate = (data, filter) => {
+      var map = new Map(JSON.parse(filter));
+      let startDate = false;
+      let endDate = false;
+      let isMatch = false
 
+      for (let [key, value] of map) {
+        if (key === 'start') {
+          let date1 = new Date((data.bookPeriod[key as keyof BookPeriod]))
+          let dd = new Date(String(value))
+          startDate = date1 >= dd;
+        }
 
+        if (key === 'end') {
+          let date1 = new Date((data.bookPeriod[key as keyof BookPeriod]))
+          let dd = new Date(String(value))
+          endDate = date1 <= dd;
+        }
 
-  ngOnInit(): void {
-//--------------------------
-    // this.getRooms()
-
-      this.dataSource.filterPredicate = (data, filter) => {
-        var map = new Map(JSON.parse(filter));
-        let startDate = false;
-        let endDate = false;
-        let isMatch = false
-
-         for (let [key, value] of map) {
-           if (key === 'start') {
-             let date1 = new Date((data.bookPeriod[key as keyof BookPeriod]).toDateString())
-             let dd = new Date(String(value))
-             startDate = date1 >= dd;
-          }
-
-           if (key === 'end') {
-             let date1 = new Date((data.bookPeriod[key as keyof BookPeriod]).toDateString())
-             let dd = new Date(String(value))
-             endDate = date1 <= dd;
-           }
-
-           if ((startDate === true && endDate === true) || value === 'All') {
-             isMatch = true;
-           }
-         }
-
-         return isMatch;
+        if ((startDate === true && endDate === true) || value === 'All') {
+          isMatch = true;
+        }
       }
+      return isMatch;
+    }
+ }
+  ngOnInit() {
+    this.getRooms()
 
 
   }
 
   getRooms() {
+    this.range.reset()
     this.httpService.getRooms().subscribe(
       (data:any)=> {
         this.rooms = data;
-        this.dataSource = new MatTableDataSource(data);
+        this.dataSource = new MatTableDataSource(this.rooms);
         this.dataSource.paginator = this.paginator || null;
         this.dataSource.sort = this.sort;
       }
-    )
+    );
   }
 
+  bookRoom(room:any) {
+    const dialogRef = this.dialog.open(RoomBookComponent, {
+      width: '60%',
+      data: room,
+    });
 
+    dialogRef.afterClosed().subscribe(result => {
+      if (result && (result.start !== null || result.end !== null) ) {
+        this.bookedRoom(room.id, result);
+      }
+    });
+
+  }
 
   applyDateFilter(event: MatDatepickerInputEvent<Date>) {
     const startDate = this.range.value.start;
@@ -105,13 +115,32 @@ export class RoomListComponent implements OnInit, AfterViewInit {
 
   showAll() {
     this.filterDictionary.set('start', 'All');
+    this.filterDictionary.set('end', 'All');
     var jsonString = JSON.stringify(Array.from(this.filterDictionary.entries()));
     this.dataSource.filter = jsonString;
     this.range.reset()
+
+    this.getRooms()
+  }
+  //запись брони
+  bookedRoom(id: number, bookPeriod: BookDates) {
+    if (bookPeriod === 'cancelBook') {
+      this.rooms.map((item) => { item.id == id ?  delete item.bookDates  : item.bookDates });
+    } else {
+      this.rooms.map((item) => { item.id == id ? item.bookDates = bookPeriod : item.bookDates });
+    }
+
+    this.httpService.bookedRoom(this.rooms).subscribe(
+      () => {
+        this.getRooms()
+      })
   }
 }
 
-/** Builds and returns a new User. */
+
+
+
+/** использовал до моков из  сервера  */
 function createNewRooms(id: number): Room {
 
   // количество мест в комнате
@@ -139,15 +168,11 @@ function createNewRooms(id: number): Room {
   };
 }
 
-
 function getRandomInt(max: any) {
   return Math.floor(Math.random() * max) + 1;
 }
 
 function randomDate(start: Date, end:Date) {
-  // console.log('start ', start);
-  // console.log('end  - ', end);
-
   return new Date(start.getTime()
     + Math.random() * (end.getTime() - start.getTime()));
 }
